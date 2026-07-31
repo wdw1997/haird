42db7f2cde8f700368a3020de07686a9350ceb30
wdw1997 <wdw19971115@outlook.com>
2026-07-31 15:54:18 +0800

    Update page.js

--- app/clients/page.js
+++ app/clients/page.js
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic'
 'use client'
+export const dynamic = 'force-dynamic'
 import { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase-client'
 import Link from 'next/link'
