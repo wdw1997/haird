950bff6d1a7fa509f4ae5e71fef8a0a53203a93a
wdw1997 <wdw19971115@outlook.com>
2026-07-31 15:54:58 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic'
 'use client'
+export const dynamic = 'force-dynamic'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase-client'
